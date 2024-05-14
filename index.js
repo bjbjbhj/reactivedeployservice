0c63971b1d6f0f2a21d304c2c6033e348ddad6cb
bjbjbhj <dsjcavid.raisch@hotmail.com>
2024-05-14 21:43:57 +0700

    init project

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const dropRightWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[arr.length - 1])) arr = arr.slice(0, -1);
-  return arr;
-};
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
+  }
+  return i === s.length;
+}
